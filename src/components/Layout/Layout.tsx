2a29b0e7f16048cffd25ddb24b8d2cdd7a4cda55
yuhur <yuhur1985@gmail.com>
2023-12-08 21:35:42 +0200

    fix: error in ts file during deploy

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,15 +1,16 @@
-import { Outlet } from "react-router-dom";
-import { ReactNode } from "react";
-import classes from "./layout.module.scss";
-import { Header } from "../../modules/headerModule";
+import React, { ReactNode } from 'react';
+import { Outlet } from 'react-router-dom';
+
+import classes from './layout.module.scss';
+import { Header } from '../../modules/headerModule';
 
 const Layout = (): ReactNode => {
-  return (
-    <main className={classes.main}>
-      <Header />
-      <Outlet />
-    </main>
-  );
+    return (
+        <main className={classes.main}>
+            <Header />
+            <Outlet />
+        </main>
+    );
 };
 
 export default Layout;
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,13 +1,13 @@
-import { ReactNode } from "react";
-import { DrawModule } from "../../modules/drawModule";
+import React, { ReactNode } from 'react';
+import { DrawModule } from '../../modules/drawModule';
 
 const HomePage = (): ReactNode => {
-  return (
-    <>
-      <h1>Home</h1>
-      <DrawModule />
-    </>
-  );
+    return (
+        <>
+            <h1>Home</h1>
+            <DrawModule />
+        </>
+    );
 };
 
 export default HomePage;
